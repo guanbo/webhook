fadc2f5cab70f3f3aff6f82102228350fdf14263
Guan Bo <guanbo2002@gmail.com>
2016-11-06 22:21:43 +0800

    Update: post support

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 
 var app = module.exports = express()
 
-app.get('/apps/:app', function (req, res) {
+app.all('/apps/:app', function (req, res) {
   var action = req.query.action||'deploy'
   console.log(new Date().toLocaleString(), req.params.app, action);
   exec(__dirname+'/bin/'+action+'.sh '+req.params.app, function () {
