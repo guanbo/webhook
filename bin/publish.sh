e38b9946a29bc727d6f67eb5fbd7a3b4e5f4ad7f
Guan Bo <guanbo2002@gmail.com>
2016-12-06 20:27:04 +0800

    Update: publish support custom source tag

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,13 +4,18 @@ cd $(dirname $0)
 
 APP=$1
 TAG=$2
+SCR_TAG="latest"
+if [ -n "$3" ]; then
+  TAG=$3
+fi
+
 IMAGE=${REGISTRY}/${DOCKER_USER}/${APP}
 
 # login docker hub
 docker login --username=${DOCKER_USERNAME} --password=${DOCKER_PASSWORD} ${REGISTRY}
 
 # push image
-docker tag ${IMAGE}:latest ${IMAGE}:${TAG}
+docker tag ${IMAGE}:${SCR_TAG} ${IMAGE}:${TAG}
 docker push ${IMAGE}:${TAG}
 
 # cleanup tag
